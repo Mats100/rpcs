f5d33b29e318479f0704c3c9f721aae35adc2c2b
Mats100 <muneeb75@icloud.com>
2023-02-27 18:16:27 +0500

    changed code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from os import environ
 from autobahn import wamp
 from models import EmployeeData
@@ -6,15 +5,13 @@ from database import session
 from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
 
 
-
 class MyComponent(ApplicationSession):
-
 
     async def onJoin(self, details):
         print("Procedure registered")
 
-        @wamp.register('com.test.create',check_types=True)
-        async def save_record(roll_id, name, phone, major):
+        @wamp.register('com.test.create', check_types=True)
+        async def save_record(roll_id: int, name: str, phone: str, major: str):
             record = EmployeeData(roll_id=roll_id, name=name, phone=phone, major=major)
             session.add(record)
             session.commit()
@@ -23,9 +20,8 @@ class MyComponent(ApplicationSession):
             result = {'status': 'success', 'data': {'roll_id': roll_id, 'name': name, 'phone': phone, 'major': major}}
             return result
 
-
         @wamp.register('com.test.get', check_types=True)
-        async def get_data(roll_id):
+        async def get_data(roll_id: int):
             data = session.query(EmployeeData).filter_by(roll_id=roll_id).first()
             session.close()
             if data:
@@ -33,7 +29,6 @@ class MyComponent(ApplicationSession):
             else:
                 result = {'msg': f" There is no student with roll ID {roll_id} ."}
             return result
-
 
 
 if __name__ == '__main__':
