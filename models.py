29160c2e370d139fb653239854097becf510ea42
Mats100 <muneeb75@icloud.com>
2023-02-28 19:28:19 +0500

    add base in models

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class EmployeeData(Base):
